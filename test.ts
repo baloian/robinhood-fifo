62835e1a81bcc3bf77528cdfcb4bd0b073657c99
Artiom Baloian <artiom.baloian@gmail.com>
2023-12-17 21:25:26 -0500

    Read <YYYYMMDD>.json files from a directory

--- test.ts
+++ test.ts
@@ -1,5 +1,17 @@
 import { abort } from '@baloian/lib';
+import { readdir } from 'fs/promises';
 import { AlpacaTax } from './alpacatax';
+
+
+async function getListOfFilenames(dirPath: string = 'inputs'): Promise<string[]> {
+  let fileNames: string[] = [];
+  try {
+    fileNames = await readdir(dirPath);
+  } catch (err) {
+    console.error('Error reading files names:', err);
+  }
+  return fileNames.sort();
+}
 
 
 /**
@@ -7,5 +19,7 @@ import { AlpacaTax } from './alpacatax';
  */
 (async () => {
   console.log(new Date(), 'Alpaca tax calculator started!');
-  AlpacaTax.calculate();
+  // AlpacaTax.calculate();
+  const files = await getListOfFilenames();
+  console.log(files);
 })().catch(abort);
