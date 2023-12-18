464179eb06b4097fd3d4e88ac334a6283f8cae4d
Artiom Baloian <artiom.baloian@gmail.com>
2023-12-18 00:42:21 -0500

    Parse and sort orders

--- alpacatax.ts
+++ alpacatax.ts
@@ -1,13 +1,14 @@
-import { getListOfFilenames, readJsonFile } from './utils';
+import { getListOfFilenames, readJsonFile, parseOrders } from './utils';
 
 
 export class AlpacaTax {
   static async calculate(dirPath: string): Promise<void> {
     const fileNames = await getListOfFilenames(dirPath);
     for (const fileName of fileNames) {
-      const jsonData = await readJsonFile(`${dirPath}/${fileName}`);
-      if (!jsonData) throw new Error(`Failed to read ${fileName} file`);
-      console.log(jsonData);
+      let fileData = await readJsonFile(`${dirPath}/${fileName}`);
+      if (!fileData) throw new Error(`Failed to read ${fileName} file`);
+      fileData = parseOrders(fileData);
+      console.log(fileData);
     }
   }
 }
--- utils.ts
+++ utils.ts
@@ -1,3 +1,4 @@
+import moment from 'moment-timezone';
 import { readFile, readdir } from 'fs/promises';
 
 
@@ -21,4 +22,18 @@ export async function readJsonFile(filePath: string): Promise<any> {
     console.error(error);
   }
   return null;
+}
+
+
+export function parseOrders(fileData: any): Promise<any> {
+  fileData.trade_activities.forEach((e: any) => {
+    const mt = moment(`${e.trade_date} ${e.trade_time}`, 'YYYY-MM-DD HH:mm:ss.SSS');
+    const nyTime = moment.tz(mt, 'America/New_York');
+    e.trade_time_ms = Number(nyTime.format('x'));
+    e.qty = Number(e.qty);
+    e.price = Number(e.price);
+    e.gross_amount = Number(e.gross_amount);
+  });
+  fileData.trade_activities = fileData.trade_activities.sort((a: any, b: any) => a.trade_time_ms - b.trade_time_ms);
+  return fileData;
 }
