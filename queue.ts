478602c63862e498ff1d46a3289baf634c9b2d99
Artiom Baloian <artiom.baloian@gmail.com>
2023-12-18 19:44:44 -0500

    Provided Queue class

---
+++ queue.ts
@@ -0,0 +1,28 @@
+export default class Queue<T> {
+  private items: T[] = [];
+
+  enqueue(item: T): void {
+    this.items.push(item);
+  }
+
+  dequeue(): T | undefined {
+    return this.items.shift();
+  }
+
+  front(): T | undefined {
+    return this.items[0];
+  }
+
+  isEmpty(): boolean {
+    return this.items.length === 0;
+  }
+
+  size(): number {
+    return this.items.length;
+  }
+
+  clear(): void {
+    this.items = [];
+  }
+}
+
