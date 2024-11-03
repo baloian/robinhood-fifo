d15d7a4c476f0f71a40467a5d9bcefadc3165bd3
Artiom Baloian <artiom.baloian@gmail.com>
2024-11-03 12:06:54 -0500

    Added testcases for the sortMonthsAndYears function

--- tests/example.test.ts
+++
@@ -1,6 +0,0 @@
-import RobinhoodFIFO from '../robinhood-fifo';
-
-test('Testing buying and selling X amount of a specific stock', async () => {
-  // await RobinhoodFIFO.run(inputDirPath, outputDirPath, false, () => {});
-  expect(2).toBe(2);
-});
